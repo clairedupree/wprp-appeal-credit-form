1a24b955fed2d458a33fabf87b07b45b9903dcf8
Claire Dupree <claire.e.dupree@gmail.com>
2022-04-18 17:59:50 -0400

    notes on data types

--- Eloquent Javascript/primitiveDataTypes.js
+++ Eloquent Javascript/primitiveDataTypes.js
@@ -2,4 +2,38 @@
 // Chapter #1
 
 //computers can only read, modify, or create new data
+//modern computers have over 30 billion bits in volatile memory
+//hard drive storage has magnitudes more
 
+//data types are called VALUES
+//create a value with name alone
+
+//NUMBER value = numeric values
+//integers and decimals
+    //64 bits
+
+//OPERATORS 
+//precedence: ( )    / * %    + -
+
+//SPECIAL VALUES
+//Infinity and -Infinity: pos and neg infinities, not mathematically sound
+//NaN: "not a number" can be numeric value but nonmeaningful ones, ex: 0/0 or Infinity-Infinity
+
+//STRING value = text
+//'single' or "double" quotes or `backticks`
+
+//SPECIAL CHARACTERS
+// \n newline
+    //can be used without escape character with `backtick` strings
+// \" quote symbol
+// \t tab
+
+//"concatenate" + "strings" + "with" + "+"
+//strings have associated methods
+//TEMPLATE LITERALS: backtick-quoted string
+//usually called template literals, can do a few more tricks. Apart from being able to span lines, they can also embed other values.
+
+//typeof returns a string naming the data type
+console.log(typeof 8)
+console.log(typeof "bob")
+console.log(typeof false)
