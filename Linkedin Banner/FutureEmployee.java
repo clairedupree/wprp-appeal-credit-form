118ace20860138c123798d1b397b548937a5c2dd
Claire Dupree <claire.e.dupree@gmail.com>
2022-06-15 16:29:34 -0400

    Renaming files, prep to make form its own repo

--- Linkedin Banner/FutureEmployee.java
+++
@@ -1,9 +0,0 @@
-public class FutureEmployee extends Perfect {
-   
-   private String name = "Claire Dupree";
-   private String gitHub = "www.github.com/clairedupree";
-   private String[] techSkills = {"java", "HTML", "CSS", "javascript", "git", "drupal"};
-   private String[] softSkills = {"communication", "diversity training", "event planning"}; 
-   private int overallRating = 5; //out of 5
-   private int humor = 6; //out of 5 
-}
