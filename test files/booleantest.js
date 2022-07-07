b629f38d0da3ea657718510a786037e11f3ca1fe
Claire Dupree <itdupr22@aacounty.org>
2022-07-07 16:21:27 -0400

    Initial commit

---
+++ test files/booleantest.js
@@ -0,0 +1,14 @@
+const boolean1 = 7 < 5;
+const boolean2 = 4 == 4;
+
+if (boolean1 == true) {
+  console.log(boolean1);
+} else {
+  console.log(boolean1);
+}
+
+const jsonData = {};
+jsonData.boolean1 = 7 < 5;
+jsonData.boolean2 = 4 == 4;
+console.log("bool1 = " + jsonData.boolean1);
+console.log("bool2 = " + jsonData.boolean2);
